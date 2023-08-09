e5a4b9764a66fac6fba5f2af3da980a038201c70
Oleh <forProgIt@gmail.com>
2023-08-09 20:22:15 +0300

    Changed gitignore file and set use for testing (TEST user can`t access for delete\insert\update something

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -1,17 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
-import { updateCurrentUser } from "../../services/apiAuth";
+// import { updateCurrentUser } from "../../services/apiAuth";
 
 export function useUpdateUser() {
   const queryClient = useQueryClient();
 
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
-    mutationFn: updateCurrentUser,
+    // mutationFn: updateCurrentUser,
     onSuccess: ({ user }) => {
       toast.success("User account successfully updated");
       queryClient.setQueryData(["user"], user);
     },
-    onError: (err) => toast.error(err.message),
+    // onError: (err) => toast.error(err.message),
+    onError: (err) => toast.error("Now you can`t update your profile"),
   });
 
   return { updateUser, isUpdating };
